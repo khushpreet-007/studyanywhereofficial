c22d20a166bb18906bd3c140e58653c399d9ddcb
Khushpreet Singh <75271300+khushpreet-007@users.noreply.github.com>
2022-06-27 16:27:56 +0530

    Update settings.py

--- codewithus/settings.py
+++ codewithus/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-jjy)5g27kr-u%2x5xnfa7fkg0uxp837ovyia&pyjzz8_r*yxy8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["studyanywhere.azurewebsites.net", "127.0.0.1"]
+
+ALLOWED_HOSTS = ['studyanywhere.azurewebsites.net', '127.0.0.1']
+
+# Application definition
+
+CSRF_TRUSTED_ORIGINS = ['https://studyanywhere.azurewebsites.net','https://127.0.0.1']
 
 
 # Application definition
